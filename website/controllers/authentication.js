ebb08af3132df6f8cff7b1a9ea20848a979e2274
truongduchuy910 <truongduchuy910@gmail.com>
2019-10-06 20:51:30 +0700

    chatbot

--- website/controllers/authentication.js
+++ website/controllers/authentication.js
@@ -1,22 +1,24 @@
-module.exports = function (app, passport) {
+var passport = require('passport')
+module.exports = function (app) {
+    require('../modules/authentication')(app, passport)
     app.post('/signup',
         function (req, res, next) {
             next();
         },
         passport.authenticate('local-signup', {
-            successRedirect: '/admin/dashboard',
+            successRedirect: '/messenger/login',
             failureRedirect: '/signup',
             failureFlash: true
         }));
-    app.post('/admin-login', passport.authenticate('local-admin-login', {
-        successRedirect: '/admin/dashboard',
-        failureRedirect: '/admin-login',
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/messenger/broadcast',
+        failureRedirect: '/login',
         failureFlash: true
     })
     );
     //------------------------------------------------------------------------------------------------------------
     app.get('/logout', function (req, res) {
         req.logout();
-        res.redirect('/admin-login');
+        res.redirect('/login');
     });
 }
