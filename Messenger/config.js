81cd85daacfade7d76d92c580245e8f14af8b21d
truongduchuy910 <truongduchuy910@gmail.com>
2019-10-30 13:15:12 +0700

    edu.loaloa.me

--- Messenger/config.js
+++ Messenger/config.js
@@ -5,6 +5,6 @@ var views = {
 }
 console.log(process.config.url)
 module.exports = {
-    PAGE_ACCESS_TOKEN: process.env.PAGE_ACCESS_TOKEN
+    PAGE_ACCESS_TOKEN: 'EAACubcIenBMBAN0kDLkpv4i4QlrDyEMkcfZBklEDIsqQsgoULabfMcioPjcXbLZCU7Tx34yZAk0ciV7pSdQPg01CRlI7L9ZC8pwPgnVOJnEBVC4BhHUgaXrLGACw0G3NOUwksjxB70lOaJFskZB7ORPrVZAQeBJGKL4XxZCJicMWrfJUP9CwpSVisjmZA3oqV30ZD'
 }
 //views.profile.persistent()
