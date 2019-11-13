9ab66aad0a52cec7914f83d4625510ec526ea18a
truongduchuy910 <truongduchuy910@gmail.com>
2019-11-13 18:36:39 +0700

    loaloa - remove upload

--- api/models.js
+++ api/models.js
@@ -4,7 +4,7 @@ module.exports = {
         var content, content_creative_id, content_id,
             attachment, attachment_creative_id, attachment_id;
         if (text) {
-            content = views.text(`Thẻ ${user} gửi ${text}`);
+            content = views.text(`Thẻ ${user} gửi: ${text}`);
             let { message_creative_id } = await broadcast.creating_broadcast_messages([content]);
             content_creative_id = message_creative_id;
             if (labels.length > 1) {
@@ -19,9 +19,9 @@ module.exports = {
             let { message_creative_id } = await broadcast.creating_broadcast_messages([attachment]);
             attachment_creative_id = message_creative_id;
             if (labels.length > 1) {
-                var { content_id } = await broadcast.sending_broadcast_messages_with_labe_predicates(attachment_creative_id, labels);
+                var { attachment_id } = await broadcast.sending_broadcast_messages_with_labe_predicates(attachment_creative_id, labels);
             } else {
-                var { content_id } = await broadcast.sending_broadcast_messages_with_labe(attachment_creative_id, labels[0]);
+                var { attachment_id } = await broadcast.sending_broadcast_messages_with_labe(attachment_creative_id, labels[0]);
 
             }
         }
