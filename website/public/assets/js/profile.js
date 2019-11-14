36095612d5a656719897b9114f8401e3eb7df2a1
truongduchuy910 <truongduchuy910@gmail.com>
2019-11-14 16:15:24 +0700

    log sdk

--- website/public/assets/js/profile.js
+++ website/public/assets/js/profile.js
@@ -192,6 +192,13 @@ routers.get_all_labels(labels => {
 
     updateContent();
 });
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'Messenger'));
 window.extAsyncInit = function () {
 
     routers.log('done loading');
