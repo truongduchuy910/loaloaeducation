cbbf92e0264f0ebc7a8674be6a8439cc9627180d
truongduchuy910 <truongduchuy910@gmail.com>
2019-11-14 16:17:27 +0700

    log sdk

--- website/public/assets/js/profile.js
+++ website/public/assets/js/profile.js
@@ -1,3 +1,11 @@
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) { return; }
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'Messenger'));
+
 
 var routers = {
     profile: function (callback) {
@@ -192,13 +200,6 @@ routers.get_all_labels(labels => {
 
     updateContent();
 });
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) { return; }
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/messenger.Extensions.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'Messenger'));
 window.extAsyncInit = function () {
 
     routers.log('done loading');
