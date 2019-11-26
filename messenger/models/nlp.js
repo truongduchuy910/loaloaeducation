2598f9df58a14c4ae7cd7863f91962defc43cdf3
truongduchuy910 <truongduchuy910@gmail.com>
2019-11-26 13:22:41 +0700

    chatbot

--- messenger/models/nlp.js
+++ messenger/models/nlp.js
@@ -6,8 +6,8 @@ module.exports = {
     message: async (text) => {
         var { entities } = await wit.message(text)
         var { request, label } = entities
-        var docs = await messenger.label.create_label(label[0].value)
-        console.log(docs)
+        var { id, error } = await messenger.label.create_label(label[0].value)
+        console.log('id: ', id, 'error: ', error)
         console.log(request, label)
     }
 }
