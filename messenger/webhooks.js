1d027440f2d1d24982d7f4b3bddc8a2be2f8c068
truongduchuy910 <truongduchuy910@gmail.com>
2019-11-26 11:46:56 +0700

    chatbot

--- messenger/webhooks.js
+++ messenger/webhooks.js
@@ -8,53 +8,48 @@ module.exports = function (app) {
                 var { message, sender, postback } = entry.messaging[0];
                 var psid = sender.id;
                 if (psid) {
-                    models.senderRecognition(psid, name => {
-                        if (name) {
-                            //   send.message(psid, views.wellcome(name))
-                        }
-                    })
-                    if (message) {
-                        models.message(message.text)
-                        //     models.message(psid, message.text)
-                    }
-                }
-                if (postback) {
-                    if (postback.payload == 'GET_STARTED') {
-                        models.senderRecognition(psid, name => {
-                            if (name) {
-                                //   send.message(psid, views.wellcome(name))
-                            }
-                        })
-                    }
-                }
-            });
 
-            res.status(200).send('EVENT_RECEIVED');
-        } else {
-            res.sendStatus(404);
+                }
+                if (message) {
+                    models.message(message.text)
+                    //     models.message(psid, message.text)
+                }
+            })
         }
 
-    })
-    app.get('/messenger/webhooks', (req, res) => {
-        console.log(req.query)
-        let VERIFY_TOKEN = "truongduc910"
-        let mode = req.query['hub.mode'];
-        let token = req.query['hub.verify_token'];
-        let challenge = req.query['hub.challenge'];
-        if (mode && token) {
+        if (postback) {
+            if (postback.payload == 'GET_STARTED') {
 
-            if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-                console.log('WEBHOOK_VERIFIED');
-                res.status(200).send(challenge);
-
-            } else {
-                console.log('WEBHOOK_VERIFI_FAIL');
-                res.sendStatus(403);
             }
+        }
+    });
+
+    res.status(200).send('EVENT_RECEIVED');
+} else {
+    res.sendStatus(404);
+}
+
+    })
+app.get('/messenger/webhooks', (req, res) => {
+    console.log(req.query)
+    let VERIFY_TOKEN = "truongduc910"
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+    if (mode && token) {
+
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
         } else {
             console.log('WEBHOOK_VERIFI_FAIL');
             res.sendStatus(403);
         }
-    });
+    } else {
+        console.log('WEBHOOK_VERIFI_FAIL');
+        res.sendStatus(403);
+    }
+});
 }
